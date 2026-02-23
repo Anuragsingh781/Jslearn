86fa9aaac2ce05e5e4fa96cb6817f7b5335a8cc3
Anuragsingh781 <149719204+Anuragsingh781@users.noreply.github.com>
2026-02-23 09:18:07 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,60 @@
+// let score = "33"
+// console.log(typeof score); // string
+// console.log(typeof (score)); // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // 33
+
+
+// let score = "33abc"
+// console.log(typeof score); // string
+// console.log(typeof (score)); // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN
+
+
+// let score = null
+// console.log(typeof score); // object
+// console.log(typeof (score)); // object 
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //0
+
+
+// let score = undefined
+// console.log(typeof score); // undefined
+// console.log(typeof (score)); // undefined
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //NaN
+
+
+// let score = true
+// console.log(typeof score); // boolean
+// console.log(typeof (score)); // boolean
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //1  0 if false
+
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
+
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // false if string is empty if there's something in the string then true
+
+
+
+let someNumber = 33 
+let StringNumber = String(someNumber)
+console.log(StringNumber);//33
+console.log(typeof StringNumber);//string
