6080445dd2d7d010fa572ff4afe723bae4021a66
Anuragsingh781 <149719204+Anuragsingh781@users.noreply.github.com>
2026-02-23 08:36:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treat all the Js code as newer version
+//alert(3 + 3)//will throw an err here since we are using node not browser. To use alert we have different syntax
+ 
+let name = "Anurag"
+let age = 22
+let isLoggedIn = true
+
+// DataTypes:->
+
+// number => 2 raise to power 53
+// bigint => not widely used but used in stocks and related things
+// string => ""
+// boolean => true/false
+// null => standalone value: representation of empty value
+// undefined => not defined yet
+// symbol => mostly seen in react (uniqueness)
+// object
+
+console.log(typeof "Anurag") // string 
+console.log(typeof isLoggedIn); // true
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
