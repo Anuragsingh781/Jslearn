e2e26fed2bde56667552acc14b98bb7921ed5acc
Anuragsingh781 <149719204+Anuragsingh781@users.noreply.github.com>
2026-02-23 17:20:48 +0000

    Conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -54,7 +54,53 @@
 
 
 
-let someNumber = 33 
-let StringNumber = String(someNumber)
-console.log(StringNumber);//33
-console.log(typeof StringNumber);//string
+// let someNumber = 33 
+// let StringNumber = String(someNumber)
+// console.log(StringNumber);//33
+// console.log(typeof StringNumber);//string
+
+
+// ************************** Operations **************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue); // -3
+
+// console.log(2 + 2); //4
+// console.log(2 - 2); //0
+// console.log(2 * 2); //4
+// console.log(2 ** 3);//8
+// console.log(2 / 3);//0.6666666
+// console.log(2 % 2);//0 modulo gives the remainder
+
+
+// let str1 = "Anurag"
+// let str2 = " Singh"
+// let str3 = str1 + str2
+// console.log(str3); // Anurag Singh
+
+// console.log("1" + 2); //12
+// console.log(1 + "2");//12
+// console.log("1" + 2 + 2); //122 👉 If a string appears first, everything after becomes string concatenation.
+// console.log(1 + 2 + "2"); // 32 👉 If numbers come first, they get added before string conversion happens.
+// // instead of these thing practise using parenthesis () which is a good practise
+
+
+console.log(+true); // 1 :Here + act as unary plus operator i.e convert value to number it is basically Number(value)
+console.log(+false);      // 0
+console.log(+null);       // 0
+console.log(+"123");      // 123
+console.log(+"abc");      // NaN
+console.log(+undefined);  // NaN
+
+
+let num1 , num2 ,num3
+num1 = num2 = num3 = 4 // not a good practise again try using 
+// let num1 = 4;
+// let num2 = 4;
+// let num3 = 4;
+
+let gameCounter = 100
+gameCounter++; // postfix operator : Use the value and then increase it
+console.log(gameCounter);//101
+++gamecounter; // prefix operator : Increase the value first and then use it
